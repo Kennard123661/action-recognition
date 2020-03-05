aa81083fec430b1be39e732e481c119e980345fe
Kennard Ng <kennard_ng@outlook.com>
2020-03-05 20:58:14 +0800

    add breakfast dataset

---
+++ src/data/breakfast.py
@@ -0,0 +1,6 @@
+import os
+from data import DATA_DIR
+
+DATASET_DIR = os.path.join(DATA_DIR, 'breakfast')
+VIDEO_DIR = os.path.join(DATASET_DIR, 'videos')
+EXTRACTED_DIR = os.path.join(DATASET_DIR, 'extracted')
