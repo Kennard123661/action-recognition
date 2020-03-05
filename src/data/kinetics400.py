da0135ee184e9e42288c4677333153359c82bd9d
kennard123661 <kennard_ng@outlook.com>
2020-03-05 10:50:35 +0800

    bugfix

--- src/data/kinetics400.py
+++ src/data/kinetics400.py
@@ -9,5 +9,5 @@ VAL_VIDEOS_CSV = os.path.join(SPLITS_DIR, 'validate.csv')
 
 VIDEO_DIR = os.path.join(DATASET_DIR, 'videos')
 TRAIN_VIDEO_DIR = os.path.join(VIDEO_DIR, 'train')
-TEST_VIDEO_DIR = os.path.join(VIDEO_DIR, 'train')
+TEST_VIDEO_DIR = os.path.join(VIDEO_DIR, 'test')
 VAL_VIDEO_DIR = os.path.join(VIDEO_DIR, 'validate')
