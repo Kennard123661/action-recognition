45d6b2802e8252d5ab53d50b90ad6b46a09d3b48
kennard123661 <kennard_ng@outlook.com>
2020-03-12 20:47:26 +0800

    add one layer mlp

--- src/nets/action_reg/baselines.py
+++ src/nets/action_reg/baselines.py
@@ -1,9 +1,19 @@
 import torch.nn as nn
+import torch
 
 
+class OneLayerMlp(nn.Module):
+    def __init__(self, in_channels, n_classes, dropout=0.5):
+        super(OneLayerMlp, self).__init__()
+        self.in_channels = in_channels
+        self.n_classes = n_classes
+        self.net = nn.Sequential(nn.Dropout(dropout),
+                                 nn.Linear(in_channels, n_classes, bias=False))
 
-# class LinearBaseline:
-#     def __init__(self, dropout=0.5):
-
-
-
+    def forward(self, inputs):
+        batch_size, n_segments, _ = inputs.shape
+        inputs = inputs.view(batch_size * n_segments, self.in_channels)
+        out = self.net(inputs)
+        out = out.view(batch_size, n_segments, self.n_classes)
+        out = torch.sum(out, dim=1)  # sum over the number of segments
+        return out
